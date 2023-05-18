13b6013aad05e78640152b9c65dd81406202a4a5
jundayw <jundayw@126.com>
2023-05-18 13:20:50 +0800

    helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,20 +1,66 @@
 <?php
 
 use Illuminate\Support\Facades\Log;
+use Jundayw\SMS\Contracts\SMSAdapterContract;
+use Jundayw\SMS\Contracts\SMSManagerContract;
+use Jundayw\SMS\Contracts\SMSResponseContract;
+use Jundayw\SMS\SMSManager;
 
 if (!function_exists('sms')) {
     /**
      * Access SMSManager through helper.
-     * @return \Jundayw\SMS\SMSManager|\Jundayw\SMS\Contracts\SMSAdapterContract
+     *
+     * @return SMSManager|SMSAdapterContract
      */
-    function sms()
+    function sms(): SMSManager|SMSAdapterContract
     {
-        return app(\Jundayw\SMS\Contracts\SMSManagerContract::class);
+        return app(SMSManagerContract::class);
     }
 }
 
-if (!function_exists('debug_logs')) {
-    function debug_logs($method)
+if (!function_exists('send_sms')) {
+    /**
+     * send sms
+     *
+     * @param mixed $to
+     * @param string $templateName
+     * @param array $templateParam
+     * @param string|null $via
+     * @param array $options
+     * @return SMSResponseContract
+     */
+    function send_sms(mixed $to, string $templateName, array $templateParam = [], string $via = null, array $options = []): SMSResponseContract
+    {
+        return app(SMSManagerContract::class)
+            ->via($via)
+            ->to($to)
+            ->scene($templateName, $templateParam)
+            ->send($options);
+    }
+}
+
+if (!function_exists('check_sms')) {
+    /**
+     * check sms
+     *
+     * @param mixed $to
+     * @param string $templateName
+     * @param mixed $input
+     * @param string|null $via
+     * @return bool
+     */
+    function check_sms(mixed $to, string $templateName, mixed $input, string $via = null): bool
+    {
+        return app(SMSManagerContract::class)
+            ->via($via)
+            ->to($to)
+            ->scene($templateName)
+            ->check($input);
+    }
+}
+
+if (!function_exists('debug_sms')) {
+    function debug_sms($method): Closure
     {
         return function ($message, array $context = []) use ($method) {
             if (config('sms.debug')) {
